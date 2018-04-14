625713f8fa8260d26f86bb54a2abf5b070adefdc
jatincoder <jatingupta2014@gmail.com>
2018-04-14 09:59:35 +0530

    Initial commit

---
+++ app/src/main/java/com/a/anew/MainActivity.java
@@ -0,0 +1,23 @@
+package com.a.anew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFx (View view) {
+
+        EditText name = (EditText) findViewById(R.id.name);
+
+        Log.i("Info", name.getText().toString());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
